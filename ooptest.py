078b3c344d5df43fc5b30de01d45784ed9636f0e
Nyancatpig <43119979+Nyancatpig@users.noreply.github.com>
2023-11-05 13:55:12 +1030

    Added 2 Python Files
    Files are currently empty

---
+++ ooptest.py
@@ -0,0 +1,8 @@
+'''
+File: ooptest.py
+Description: A brief description of this Python module.
+Author: Bailey Csortan
+StudentID: 110409234
+EmailID: csoby001
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
