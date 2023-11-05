5158fee8dfd5309a5ec8a40f3485a7b0bb617381
Nyancatpig <43119979+Nyancatpig@users.noreply.github.com>
2023-11-05 13:56:41 +1030

    Added Laboratory Class

--- oopmain.py
+++ oopmain.py
@@ -7,3 +7,15 @@ EmailID: csoby001
 This is my own work as defined by the University's Academic Misconduct Policy.
 '''
 
+class Laboratory():
+    '''Laboratory Class, Owns other Classes'''
+    def __init__(self):
+        self.__potions = []
+        self.__herbs = []
+        self.__catalysts = []
+    
+    def mixPotion(self, name, type, stat, primaryIngredient, secondaryIngredient):
+        pass
+
+    def addReagent(self, reagent, amount):
+        pass
